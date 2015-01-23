105a0165780578b5e1b5412f0ded0f8d495d755a
SamuraiSusy <susyy-@hotmail.com>
2015-01-23 23:48:10 +0200

    user interface
    ui started, buttons are a mess right now

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actions : MonoBehaviour
+{
+    public GameObject UIObject;
+    public int actionCount = 7;
+    public int[] actions;
+
+	// Use this for initialization
+	void Start ()
+    {
+        UIObject = GameObject.FindWithTag("UI object");
+        actions = new int[actionCount];
+
+        for(int i = 0; i < actionCount; i++)
+        {
+            actions[i] = i;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void ChooseAction()
+    {
+        if(actions[actionCount] == 1)
+        {
+            DoSomething1();
+        }
+        else if(actions[actionCount] == 2)
+        {
+            DoSomething2();
+        }
+        else if (actions[actionCount] == 3)
+        {
+            DoSomething3();
+        }
+        else if (actions[actionCount] == 4)
+        {
+            DoSomething4();
+        }
+        else
+        {
+            Debug.Log("something else");
+        }
+    }
+
+    void DoSomething1()
+    {
+        if (actions[actionCount] == 1)
+        {
+            Debug.Log("action #" + actionCount);
+        }
+    }
+
+    void DoSomething2()
+    {
+        if (actions[actionCount] == 2)
+        {
+            Debug.Log("action #" + actionCount);
+        }
+    }
+
+    void DoSomething3()
+    {
+        if (actions[actionCount] == 3)
+        {
+            Debug.Log("action #" + actionCount);
+        }
+    }
+
+    void DoSomething4()
+    {
+        if (actions[actionCount] == 4)
+        {
+            Debug.Log("action #" + actionCount);
+        }
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour
+{
+    public GameObject ActionObject;
+    public int[] buttonIDs;
+    public GameObject[] actionButtons;
+    public GameObject buttonPref;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ActionObject = GameObject.FindWithTag("Action object");
+        Actions juttu = ActionObject.gameObject.AddComponent<Actions>();
+        float osx = 0;
+        float osy = 0.3f;
+        actionButtons = new GameObject[6];
+
+        // NOT WORKING ANYMORE
+        for (int i = 0; i < juttu.actionCount; ++i)
+        {
+            // CAUSE OF THIS
+            if(juttu.actionCount < 3)
+            {
+                actionButtons[i] = (GameObject)Instantiate(buttonPref, new Vector3(1f + osx, 0.1f), Quaternion.identity);
+            }
+            else if(juttu.actionCount >= 3 && juttu.actionCount < 7)
+            {
+                actionButtons[i] = (GameObject)Instantiate(buttonPref, new Vector3(0.1f + osx, 0.1f + osy), Quaternion.identity);
+            }
+
+            osx += 0.3f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        //float offSet = 0;
+        //for(int i = 0; i < buttonIDs.Length; i++)
+        //{
+        //    if (GUI.Button(new Rect(10, 10 + offSet, 100, 50), "mo"))
+        //    {
+        //        CallButton(buttonIDs[i]);
+        //    }
+
+        //    offSet += 60;
+        //}
+    }
+
+    void CallButton(int buttonID)
+    {
+        //buttonID = 
+        //Debug.Log("pressed" + buttonID);
+    }
+}
