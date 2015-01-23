451c074e27bfc848529fedc0c9d86decf7908807
SamuraiSusy <susyy-@hotmail.com>
2015-01-23 20:56:47 +0200

    basic project

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        Screen.SetResolution(720, 1280, true);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
