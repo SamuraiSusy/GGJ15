1e7e0befaa5dc2ffcd53cea3d4cbb0c66ed92446
TheMetabug <metabug@gmail.com>
2015-01-25 15:08:24 +0200

    Updated main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	//public int buttonCount = 3;
+	//public GameObject   buttonPref;
+	//public GameObject[] ActionButtons;
+	// Use this for initialization
+	void Start () 
+	{
+		/*
+		float os1 = 0;
+		float os2 = 0f;
+		ActionButtons = new GameObject[buttonCount];
+
+		for (int i = 0; i < buttonCount; ++i) {
+						if (i < 3) {
+								ActionButtons [i] = (GameObject)Instantiate (buttonPref, new Vector3 (-1.7f + os1, -3.85f), Quaternion.identity);
+								os1 += 1.7f;
+								//TODO: put own GUITEXT to own thing
+						}
+				}
+
+		*/
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0))
+						Application.LoadLevel ("testi");
+
+	}
+}
